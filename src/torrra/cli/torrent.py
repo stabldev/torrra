104114934edc8d14cac2f2447762284b24c604a7
stabldev <114811070+stabldev@users.noreply.github.com>
2025-07-13 11:12:11 +0530

    fix: use download config options

--- src/torrra/cli/torrent.py
+++ src/torrra/cli/torrent.py
@@ -7,55 +7,60 @@ from questionary import Choice
 from rich.console import Console
 
 from torrra.constants import UI_STRINGS
+from torrra.core.config import Config
 from torrra.core.downloader import download_magnet
 from torrra.indexers import INDEXERS_MAP
 from torrra.types import Torrent
 from torrra.utils import styles, welcome
 
 console = Console()
+config = Config()
 
 
 def run_torrent_flow() -> None:
     welcome.show_welcome()
 
-    query = questionary.text(UI_STRINGS["prompt_search_query"], style=styles.TEXT).ask()
-    if not query:
-        return
+    # query = questionary.text(UI_STRINGS["prompt_search_query"], style=styles.TEXT).ask()
+    # if not query:
+    #     return
 
-    indexer_name = questionary.select(
-        UI_STRINGS["prompt_choose_indexer"],
-        choices=list(INDEXERS_MAP.keys()),
-        style=styles.SELECT,
-    ).ask()
-    if not indexer_name:
-        return
+    # indexer_name = questionary.select(
+    #     UI_STRINGS["prompt_choose_indexer"],
+    #     choices=list(INDEXERS_MAP.keys()),
+    #     style=styles.SELECT,
+    # ).ask()
+    # if not indexer_name:
+    #     return
 
-    indexer_module_path = INDEXERS_MAP[indexer_name]
-    indexer = importlib.import_module(indexer_module_path).Indexer()
+    # indexer_module_path = INDEXERS_MAP[indexer_name]
+    # indexer = importlib.import_module(indexer_module_path).Indexer()
 
-    with console.status(
-        UI_STRINGS["status_searching"].format(indexer=indexer_name, query=query)
-    ):
-        torrents: List[Torrent] = indexer.search(query)
+    # with console.status(
+    #     UI_STRINGS["status_searching"].format(indexer=indexer_name, query=query)
+    # ):
+    #     torrents: List[Torrent] = indexer.search(query)
 
-    if not torrents:
-        console.print(UI_STRINGS["error_no_results"])
-        return
+    # if not torrents:
+    #     console.print(UI_STRINGS["error_no_results"])
+    #     return
 
-    torrent_choices = [
-        Choice(title=torrent.title, value=torrent) for torrent in torrents
-    ]
+    # torrent_choices = [
+    #     Choice(title=torrent.title, value=torrent) for torrent in torrents
+    # ]
 
-    selected_torrent: Torrent | None = questionary.select(
-        UI_STRINGS["prompt_select_result"],
-        choices=torrent_choices,
-        style=styles.SELECT,
-    ).ask()
-    if not selected_torrent:
-        return
+    # selected_torrent: Torrent | None = questionary.select(
+    #     UI_STRINGS["prompt_select_result"],
+    #     choices=torrent_choices,
+    #     style=styles.SELECT,
+    # ).ask()
+    # if not selected_torrent:
+    #     return
+
+    default_download_path = config.get("general.download_path")
 
     save_path = questionary.path(
         UI_STRINGS["prompt_download_path"],
+        default=default_download_path,
         only_directories=True,
         complete_style=CompleteStyle.COLUMN,
         style=styles.TEXT,
@@ -63,5 +68,8 @@ def run_torrent_flow() -> None:
     if not save_path:
         return
 
+    if save_path != default_download_path and config.get("general.remember_last_path"):
+        config.set("general.download_path", save_path)
+
     # TODO: handle config
-    download_magnet(selected_torrent.magnet_uri, save_path)
+    # download_magnet(selected_torrent.magnet_uri, save_path)
