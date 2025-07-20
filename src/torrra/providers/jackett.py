111e57d11cf6c942129b42b0e2c33cc135c0ef84
stabldev <114811070+stabldev@users.noreply.github.com>
2025-07-20 20:49:45 +0530

    chore: update error msgs

--- src/torrra/providers/jackett.py
+++ src/torrra/providers/jackett.py
@@ -27,12 +27,21 @@ class JackettClient:
                 resp = await client.get(url, params=params)
                 resp.raise_for_status()
                 return True
-            except httpx.RequestError as e:
-                print(f"[error] jackett: connection error: {e}")
+
+            except httpx.RequestError:
+                print(f"[error] could not connect to jackett at {self.url}.")
+                print("please make sure jackett is running and the url is correct.")
+
             except httpx.HTTPStatusError as e:
                 status_code = e.response.status_code
+
                 if status_code == 401:
-                    print("[error] jackett: invalid api key")
+                    print("[error] invalid jackett api key.")
+                    print("double-check the api key you provided.")
                 elif status_code == 500 and "nonexistent_indexer" in e.response.text:
                     return True
+                else:
+                    print(f"[error] jackett returned http {status_code}.")
+                    print("unexpected response from jackett. please verify your setup.")
+
             return False
