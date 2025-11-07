304f47c4248722ca380320e8ec3a01b4fc0c1206
stabldev <114811070+stabldev@users.noreply.github.com>
2025-11-07 17:55:13 +0530

    chore: update deps and change default theme

--- src/torrra/app.py
+++ src/torrra/app.py
@@ -3,6 +3,7 @@ from typing import ClassVar
 from textual import work
 from textual.app import App
 from textual.binding import BindingType
+from textual.reactive import Reactive
 from textual.types import CSSPathType
 
 from torrra._types import Indexer
@@ -11,14 +12,14 @@ from torrra.utils.fs import get_resource_path
 
 
 class TorrraApp(App[None]):
+    theme: Reactive[str] = Reactive("textual-dark")
+
     TITLE: str | None = "torrra"
     CSS_PATH: ClassVar[CSSPathType | None] = get_resource_path("app.css")
+    ENABLE_COMMAND_PALETTE: ClassVar[bool] = False
     BINDINGS: ClassVar[list[BindingType]] = [
-        ("q", "quit", "Quit"),
-        ("d", "toggle_dark_mode", "Toggle dark mode"),
         ("escape", "clear_focus", "Clear focus"),
     ]
-    ENABLE_COMMAND_PALETTE: ClassVar[bool] = False
 
     def __init__(self, provider: Indexer | None, use_cache: bool) -> None:
         super().__init__()
@@ -27,25 +28,13 @@ class TorrraApp(App[None]):
 
     @work
     async def on_mount(self) -> None:
-        self.theme = (  # pyright: ignore[reportUnannotatedClassAttribute]
-            "catppuccin-mocha"
-        )
+        self.theme = "gruvbox"
 
         if query := await self.push_screen_wait(WelcomeScreen(provider=self.provider)):
             from torrra.screens.search import SearchScreen
 
-            await self.push_screen(
-                SearchScreen(
-                    indexer=self.provider, initial_query=query, use_cache=self.use_cache
-                )
-            )
-
-    def action_toggle_dark_mode(self) -> None:
-        self.theme = (
-            "catppuccin-mocha"
-            if self.theme == "catppuccin-latte"
-            else "catppuccin-latte"
-        )
+            search_screen = SearchScreen(indexer=self.provider, query=query)
+            await self.push_screen(search_screen)
 
     def action_clear_focus(self) -> None:
         self.set_focus(None)
