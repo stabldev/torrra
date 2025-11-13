d3fb28cd82e452af1fdf3b6db1461b242334af94
stabldev <114811070+stabldev@users.noreply.github.com>
2025-11-13 22:29:24 +0530

    chore: patch v1.3.2 release

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "Torrra"
 copyright = "2025, stabldev"
 author = "stabldev"
-release = "1.3.1"
+release = "1.3.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
