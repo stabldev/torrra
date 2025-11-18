2682f48ee83a13152785f6f413cec2d181be643d
stabldev <114811070+stabldev@users.noreply.github.com>
2025-11-18 21:46:09 +0530

    chore: patch v1.3.5 release

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "Torrra"
 copyright = "2025, stabldev"
 author = "stabldev"
-release = "1.3.4"
+release = "1.3.5"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
