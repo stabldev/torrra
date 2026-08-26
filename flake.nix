21cf7ab612ce6af9ceec28a60c55eefd452c858f
stabldev <114811070+stabldev@users.noreply.github.com>
2026-08-26 14:31:43 +0530

    chore: bump version to v2.3.2

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
   outputs = { self, nixpkgs }:
     let
-      version = "2.3.1";
+      version = "2.3.2";
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSystem = f: nixpkgs.lib.genAttrs systems (system:
         f nixpkgs.legacyPackages.${system}
